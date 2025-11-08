e4b5006777950ad5abb8d812260fa32007bcc02b
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-11-08 23:03:36 +0530

    solve typo mistake

--- Ecom_frontend/src/Components/Navbar.jsx
+++ Ecom_frontend/src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from '../CSS/navbar.module.css';
 import { FiShoppingCart } from 'react-icons/fi'; 
 
@@ -7,11 +8,11 @@ const Navbar = () => {
       <nav className={styles.navbar}>
         <div className={styles.logo}>Nexora Commerce</div>
         <ul className={styles.navLinks}>
-          <li><a href="/" >Home</a></li>
-          <li><a href="/cart">Cart</a></li>
+          <li><Link to="/" >Home</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
         </ul>
         <div className={styles.cartIcon}>
-          <li><a href="/cart"><FiShoppingCart size={24} /></a></li>
+          <li><Link to="/cart"><FiShoppingCart size={24} /></Link></li>
         </div>
       </nav>
     </header>
