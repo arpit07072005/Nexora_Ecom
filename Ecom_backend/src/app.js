b44613ba74e0d953ee17177e305cbdc882aa4deb
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-11-08 22:45:11 +0530

    frontend url updated

--- Ecom_backend/src/app.js
+++ Ecom_backend/src/app.js
@@ -4,7 +4,7 @@ import router from './Route/route.js';
 const app=express();
 
 app.use(cors({
-    origin:" http://localhost:5173",
+    origin:"https://nexora-ecom.vercel.app",
     credentials:true
 }))
 app.use(express.json({limit:"16kb"}));
